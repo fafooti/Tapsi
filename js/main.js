f87c24e256d3315fc7cb9f03e0716da80702426c
fafooti <75925429+fafooti@users.noreply.github.com>
2023-08-03 23:41:54 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+
+    navbar.classList.toggle("nav__expanded");
+});
+
+
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        const targetTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+        tab.classList.add("active");
+        targetTabContent.classList.add("active")
+    })
+})
